8924b07c8548edc85387d9b924bfd3f5b1b78ef7
Asgarrrr <caruellej@hotmail.fr>
2024-11-04 00:42:21 +0100

    Refactor AppThemeSwitcher component to AppThemeProvider and remove unused code

--- src/components/theme/index.tsx
+++ src/components/theme/index.tsx
@@ -2,47 +2,7 @@
 
 import type React from "react";
 
-import { cn } from "@/lib/cn";
-
-import { ThemeProvider, useTheme } from "next-themes";
-import { useEffect, useState } from "react";
-
-// export const AppThemeSwitcher = () => {
-
-//     const [ mounted, setMounted ] = useState( false );
-//     const { theme, setTheme } = useTheme();
-
-//     useEffect(() => {
-//         setMounted(true);
-//     }, []);
-
-//   if (!mounted) return null;
-
-//   const buttons = [
-//     {
-//       label: "system",
-//       icon: <Monitor width={13} />,
-//       active: theme === "system",
-//     },
-//     { label: "dark", icon: <Moon width={13} />, active: theme === "dark" },
-//     { label: "light", icon: <Sun width={13} />, active: theme === "light" },
-//   ];
-
-//   return (
-//     <span className="flex w-fit items-center gap-0.5 overflow-hidden rounded-[6px] bg-gray-2 p-[2px]">
-//       {buttons.map(({ label, icon, active }) => (
-//         <button
-//           type="button"
-//           key={label}
-//           onClick={() => setTheme(label)}
-//           className={cn("ransition-all flex h-6 w-6 items-center justify-center rounded-[4px] hover:opacity-50", active ? "bg-gray-4 text-foreground" : "")}
-//         >
-//           {icon}
-//         </button>
-//       ))}
-//     </span>
-//   );
-// };
+import { ThemeProvider } from "next-themes";
 
 export const AppThemeProvider = ({
     children,
